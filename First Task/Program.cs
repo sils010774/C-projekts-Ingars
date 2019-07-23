afb6a089732d2cfcc1eba06ffad090dda5b843d1
unknown <sales@beautyfloor.eu>
2019-07-23 20:02:30 +0300

    init

---
+++ First Task/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace First_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is yor name:?");
+
+
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine(name);
+            Console.Write("What is yor Surname:?");
+
+            string Surname = Console.ReadLine();
+
+
+
+            //Console.WriteLine(Surname);
+            //Console.WriteLine(name + " " + Surname);
+            //Console.WriteLine($"Your full name is: {name} {Surname}");
+            //Console.WriteLine(string.Format("Your full name is: {name} {Surname}");
+
+
+            //int x;
+            //int y = 1;
+
+            //x = 5;
+            //y = x + 3;
+
+            //int z = x + y;
+
+            //Console.WriteLine(y);
+
+            //var t = y + x;
+            //var name = "Ingars";
+
+            //var test = 23;
+
+            //double testdouble = 12;
+            //int testInt = 33;
+
+            //var testResult = testdouble + testInt;
+
+            //var testResult2 = name + testdouble;
+
+            //Console.WriteLine(testResult2);
+          
+
+
+
+
+
+
+
+
+        }
+    }
+}
